559997256c1980ed155246fcfbf929ad9d2f7c9a
Oliver Clarke <oclarke@routeware.com>
2026-01-27 20:28:09 +0000

    Add Scalar docs

--- src/GeoTrack-API/GeoTrack-API/Program.cs
+++ src/GeoTrack-API/GeoTrack-API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    // Scalar UI for the OpenAPI document
+    // Default route: /scalar/v1 (Scalar will read from the mapped OpenAPI endpoint)
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
